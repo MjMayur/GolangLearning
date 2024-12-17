66d65815a219d5ee1ccfc38681e77ae788de15fa
Mayur Jadhav <121611064+MjMayur@users.noreply.github.com>
2024-12-17 19:18:01 +0530

    Add files via upload

--- GoCource/Practice/main.go
+++ GoCource/Practice/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	// "database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 
 	// "strconv"
 	// "time"
@@ -46,6 +48,9 @@ func main() {
 	router := gin.Default()
 	router.Use(CORSMiddleware())
 	router.POST("/user/create/", CreateUserHandler)
+	router.GET("/user/list/", ListUser)
+	router.DELETE("/user/delete/:id", DeleteUser)
+	router.GET("/user/get/:id", GetDetails)
 	router.Run(":8000")
 }
 
@@ -62,14 +67,81 @@ func ConnectDatabase() {
 	}
 }
 
-func CreateUserHandler(c *gin.Context) {
+type responseStruct struct {
+	Code    int    `json:"code"`
+	Massage string `json:"massage"`
+}
 
-	type createUserRequest struct {
-		Name    string `json:"name" binding:"required"`
-		Email   string `json:"email" binding:"required"`
-		Phone   int    `json:"phone" binding:"required"`
-		Message string `json:"message" binding:"required"`
+type createUserRequest struct {
+	Name    string `json:"name" binding:"required"`
+	Email   string `json:"email" binding:"required"`
+	Phone   string `json:"phone" binding:"required"`
+	Message string `json:"message" binding:"required"`
+}
+
+func validationEmail(email string) error {
+	// Regular expression for validating an email
+
+	var emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+
+	if !re.MatchString(email) {
+		return errors.New("invalid email format")
 	}
+	return nil
+}
+
+type listRes struct {
+	Id      int    `db:"id"`
+	Name    string `db:"name"`
+	Email   string `db:"email"`
+	Phone   string `db:"phone"`
+	Message string `db:"message"`
+}
+
+type tempResponse struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Phone   string `json:"phone"`
+	Message string `json:"message"`
+}
+
+func ListUser(c *gin.Context) {
+
+	tempModels := []listRes{}
+
+	query := "SELECT * FROM users"
+	err = db.Select(&tempModels, query)
+	if err != nil {
+		log.Println("Email validation error:", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	response := []tempResponse{}
+
+	for _, tempModel := range tempModels {
+		response = append(response, tempResponse{
+			ID:      tempModel.Id,
+			Name:    tempModel.Name,
+			Phone:   tempModel.Phone,
+			Email:   tempModel.Email,
+			Message: tempModel.Message,
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"Users":   response,
+		"code":    200,
+		"message": "Success recorder added successfully.",
+	})
+}
+
+func CreateUserHandler(c *gin.Context) {
 
 	var request createUserRequest
 
@@ -82,8 +154,12 @@ func CreateUserHandler(c *gin.Context) {
 	// Validate email format
 	if err := validationEmail(request.Email); err != nil {
 		log.Println("Email validation error:", err)
-		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
 		return
+
 	}
 	query := "INSERT INTO users (name, email, phone, message) VALUES (?, ?, ?, ?)"
 	result, err := db.Exec(query, request.Name, request.Email, request.Phone, request.Message)
@@ -100,18 +176,117 @@ func CreateUserHandler(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"SUCCESS": userID})
+	c.JSON(http.StatusOK, gin.H{
+		"SUCCESS": userID,
+		"code":    200,
+		"message": "Success recorder added successfully.",
+	})
 }
 
-func validationEmail(email string) error {
-	// Regular expression for validating an email
-	var emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
-	re := regexp.MustCompile(emailRegex)
-
-	if !re.MatchString(email) {
-		return fmt.Errorf("invalid email format")
+func DeleteUser(c *gin.Context) {
+	userIDstr := c.Param("id")
+	userID, err := strconv.Atoi(userIDstr)
+	if err != nil {
+		log.Println("Email validation error:", err)
+		c.JSON(http.StatusForbidden, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
 	}
-	return nil
+
+	query := "DELETE FROM users WHERE id=?"
+	_, err = db.Exec(query, userID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusForbidden, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":    200,
+		"message": "Record Deleted successfully",
+	})
+}
+
+func GetDetails(c *gin.Context) {
+	userIDstr := c.Param("id")
+	userID, err := strconv.Atoi(userIDstr)
+	if err != nil {
+		log.Println("Email validation error:", err)
+		c.JSON(http.StatusForbidden, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+	tempModels := listRes{}
+
+	query := "SELECT * FROM users WHERE id=?"
+	err = db.Get(&tempModels, query, userID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
+		return
+	}
+
+	response := tempResponse{
+		ID:      tempModels.Id,
+		Name:    tempModels.Name,
+		Phone:   tempModels.Phone,
+		Email:   tempModels.Email,
+		Message: tempModels.Message,
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"User":    response,
+		"code":    200,
+		"message": "Success recorder added successfully.",
+	})
+}
+
+func UpdateUser(c *gin.Context) {
+	userIDstr := c.Param("id")
+	userID, err := strconv.Atoi(userIDstr)
+	if err != nil {
+		log.Println("Email validation error:", err)
+		c.JSON(http.StatusForbidden, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	var request createUserRequest
+	err = c.ShouldBindJSON(&request)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
+		return
+	}
+	// Validate email format
+	if err := validationEmail(request.Email); err != nil {
+		log.Println("Email validation error:", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+
+	}
+	query := "UPDATE users SET name=?, phone=?, email=?,message=? WHERE id=?"
+	_, err = db.Exec(query, request.Name, request.Phone, request.Email, request.Message, userID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code":    200,
+		"message": "Success recorder Updated successfully.",
+	})
 }
 
 // func GetUserDetails(c *gin.Context) {
