658bc2f51349af6420f04ae9ff3604289a1faab8
Mayur Jadhav <121611064+MjMayur@users.noreply.github.com>
2024-12-17 19:16:38 +0530

    Add files via upload

--- GoCource/main.go
+++ GoCource/main.go
@@ -1,423 +1,280 @@
 package main
 
 import (
-	// "database/sql"
-	"errors"
+	"bufio"
 	"fmt"
 	"log"
-	"net/http"
-	"regexp"
-	"strconv"
-
-	// "strconv"
-	// "time"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
+	"net"
+	"strings"
 )
 
-const (
-	DRIVER    = "mysql"
-	MYSQLPORT = "3306"
-	HOST      = "localhost"
-	USER      = "root"
-	PASSWORD  = "password"
-	DBNAME    = "project"
-)
+//############## ------First Lecture --------##############//
+// // visible inside the package only
+// // type person struct {
+// // 	fname string
+// // 	lname string
+// // }
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+// // type secretAgent struct {
+// // 	person
+// // 	kill bool
+// // }
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
+// // for visible outside the package take first letter capital of variables.
+// //
+// //	type person struct {
+// //		Fname string
+// //		Lname string
+// //	}
+// // type human interface {
+// // 	write()
+// // 	// speak()
 
-		c.Next()
-	}
-}
+// // }
+
+// type calculateArea interface {
+// 	calculate()
+// }
+
+// type circle struct {
+// 	r float32
+// 	b int
+// }
+
+// type square struct {
+// 	side1 float32
+// }
+
+// func main() {
+// 	// xi := []int{2, 3, 4, 5, 6}
+// 	// fmt.Println(xi)
+
+// 	// m := map[string]int{
+// 	// 	"joe": 41,
+// 	// 	"toe": 51,
+// 	// }
+// 	// fmt.Println(m)
+
+// 	// p1 := person{
+// 	// 	"mayur",
+// 	// 	"jadhav",
+// 	// }
+
+// 	// sa1 := secretAgent{
+// 	// 	person{
+// 	// 		"James",
+// 	// 		"bond",
+// 	// 	},
+// 	// 	true,
+// 	// }
+// 	radius := circle{
+// 		10,
+// 		12,
+// 	}
+// 	square := square{
+// 		3.4,
+// 	}
+
+// 	// fmt.Println(p1)
+// 	// p1.write()
+// 	// sa1.speck()
+// 	// saySomething(sa1)
+// 	// saySomething(p1)
+// 	calculateBothArea(radius)
+// 	calculateBothArea(square)
+// 	// radius.calculate()
+// 	// square.calculate()
+
+// }
+
+// // functions in go
+// // func (p person) write() {
+// // 	fmt.Println(p.fname, `Hii how are you`)
+// // }
+
+// // func (sa secretAgent) write() {
+// // 	fmt.Println(sa.fname, sa.lname, "will kill you", sa.kill)
+// // }
+// // func saySomething(h human) {
+// // 	// h.speak()
+// // 	h.write()
+// // }
+
+// // Method to calculate circumference
+// func (r circle) calculate() {
+// 	const pie = 3.14
+// 	circumference := 2 * pie * r.r
+// 	fmt.Printf("Circumference of the circle: %.2f\n", circumference)
+// }
+
+// func (s1 square) calculate() {
+// 	value := s1.side1 * s1.side1
+// 	fmt.Printf("Circumference of the circle: %.2f\n", value)
+// }
+
+// func calculateBothArea(r calculateArea) {
+// 	r.calculate()
+// }
+
+//############## ------Third Lecture Server Creation  --------##############//
 
 func main() {
-	ConnectDatabase()
-
-	router := gin.Default()
-	router.Use(CORSMiddleware())
-	router.POST("/user/create/", CreateUserHandler)
-	router.GET("/user/list/", ListUser)
-	router.DELETE("/user/delete/:id", DeleteUser)
-	router.GET("/user/get/:id", GetDetails)
-	router.Run(":8000")
-}
-
-var db *sqlx.DB
-var err error
-
-func ConnectDatabase() {
-	// connectionString := "USER:PASSWORD@tcp(HOST:MYSQLPORT)/DBNAME"
-	connectionStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", USER, PASSWORD, HOST, MYSQLPORT, DBNAME)
-	db, err = sqlx.Open(DRIVER, connectionStr)
+	li, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatal(err)
-		return
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+		// io.WriteString(conn, "\nHello TCP Server Mayur is here ky mhanato bhava")
+		// fmt.Fprintln(conn, "How are you connection")
+		// fmt.Fprintf(conn, "%v", "Well Done")
+		// conn.Close()
 	}
 }
 
-type responseStruct struct {
-	Code    int    `json:"code"`
-	Massage string `json:"massage"`
+func handle(conn net.Conn) {
+	// err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	// if err != nil {
+	// 	log.Println("CONN TIMEOUT")
+	// }
+	// scanner := bufio.NewScanner(conn)
+	// for scanner.Scan() {
+	// 	ln := scanner.Text()
+	// 	fmt.Println(ln)
+	// 	fmt.Fprintf(conn, "I heard you say : %s\n", ln)
+	// }
+	defer conn.Close()
+	fmt.Println("Code is here ")
+	request(conn)
 }
 
-type createUserRequest struct {
-	Name    string `json:"name" binding:"required"`
-	Email   string `json:"email" binding:"required"`
-	Phone   string `json:"phone" binding:"required"`
-	Message string `json:"message" binding:"required"`
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			mux(conn, ln)
+		}
+		if ln == "" {
+			break
+		}
+		i++
+	}
 }
 
-func validationEmail(email string) error {
-	// Regular expression for validating an email
+// #############-------TCP server Mux server
+func mux(conn net.Conn, ln string) {
+	m := strings.Fields(ln)[0]
+	u := strings.Fields(ln)[1]
+	fmt.Println("****Method", m)
+	fmt.Println("****URI", u)
 
-	var emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
-	re := regexp.MustCompile(emailRegex)
-
-	if !re.MatchString(email) {
-		return errors.New("invalid email format")
+	//multiplexer
+	if m == "GET" && u == "/" {
+		index(conn)
 	}
-	return nil
+	if m == "GET" && u == "/about" {
+		about(conn)
+	}
+	if m == "GET" && u == "/contact" {
+		contact(conn)
+	}
+	if m == "GET" && u == "/apply" {
+		apply(conn)
+	}
+	if m == "POST" && u == "/apply" {
+		applyProcess(conn)
+	}
+
 }
 
-type listRes struct {
-	Id      int    `db:"id"`
-	Name    string `db:"name"`
-	Email   string `db:"email"`
-	Phone   string `db:"phone"`
-	Message string `db:"message"`
+func index(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta
+charet="UTF-8"><title></title></head><body>
+<strong>INDEX</strong><br>
+<a href="/">index</a><br>
+<a href="/about">about</a><br>
+<a href="/contact">contact</a><br>
+<a href="/apply">apply</a><br>
+</body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
 
-type tempResponse struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Phone   string `json:"phone"`
-	Message string `json:"message"`
+func about(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta
+charet="UTF-8"><title></title></head><body>
+<strong>About</strong><br>
+<a href="/">index</a><br>
+<a href="/about">about</a><br>
+<a href="/contact">contact</a><br>
+<a href="/apply">apply</a><br>
+</body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
 
-func ListUser(c *gin.Context) {
-
-	tempModels := []listRes{}
-
-	query := "SELECT * FROM users"
-	err = db.Select(&tempModels, query)
-	if err != nil {
-		log.Println("Email validation error:", err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-	}
-
-	response := []tempResponse{}
-
-	for _, tempModel := range tempModels {
-		response = append(response, tempResponse{
-			ID:      tempModel.Id,
-			Name:    tempModel.Name,
-			Phone:   tempModel.Phone,
-			Email:   tempModel.Email,
-			Message: tempModel.Message,
-		})
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"Users":   response,
-		"code":    200,
-		"message": "Success recorder added successfully.",
-	})
+func contact(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta
+charet="UTF-8"><title></title></head><body>
+<strong>Contact</strong><br>
+<a href="/">index</a><br>
+<a href="/about">about</a><br>
+<a href="/contact">contact</a><br>
+<a href="/apply">apply</a><br>
+</body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
 
-func CreateUserHandler(c *gin.Context) {
-
-	var request createUserRequest
-
-	err = c.ShouldBindJSON(&request)
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
-		return
-	}
-	// Validate email format
-	if err := validationEmail(request.Email); err != nil {
-		log.Println("Email validation error:", err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-
-	}
-	query := "INSERT INTO users (name, email, phone, message) VALUES (?, ?, ?, ?)"
-	result, err := db.Exec(query, request.Name, request.Email, request.Phone, request.Message)
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-		return
-	}
-
-	userID, err := result.LastInsertId()
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"Last Insert ID getting Error": err})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"SUCCESS": userID,
-		"code":    200,
-		"message": "Success recorder added successfully.",
-	})
+func apply(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta
+charet="UTF-8"><title></title></head><body>
+<strong>APPLY</strong><br>
+<a href="/">index</a><br>
+<a href="/about">about</a><br>
+<a href="/contact">contact</a><br>
+<a href="/apply">apply</a><br>
+<form method="post" action="/apply">
+<input type="submit" value="apply">
+</form>
+</body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
 
-func DeleteUser(c *gin.Context) {
-	userIDstr := c.Param("id")
-	userID, err := strconv.Atoi(userIDstr)
-	if err != nil {
-		log.Println("Email validation error:", err)
-		c.JSON(http.StatusForbidden, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-	}
-
-	query := "DELETE FROM users WHERE id=?"
-	_, err = db.Exec(query, userID)
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusForbidden, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"code":    200,
-		"message": "Record Deleted successfully",
-	})
+func applyProcess(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta
+charet="UTF-8"><title></title></head><body>
+<strong>APPLY PROCESS</strong><br>
+<a href="/">index</a><br>
+<a href="/about">about</a><br>
+<a href="/contact">contact</a><br>
+<a href="/apply">apply</a><br>
+</body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
-
-func GetDetails(c *gin.Context) {
-	userIDstr := c.Param("id")
-	userID, err := strconv.Atoi(userIDstr)
-	if err != nil {
-		log.Println("Email validation error:", err)
-		c.JSON(http.StatusForbidden, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-	}
-	tempModels := listRes{}
-
-	query := "SELECT * FROM users WHERE id=?"
-	err = db.Get(&tempModels, query, userID)
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-		return
-	}
-
-	response := tempResponse{
-		ID:      tempModels.Id,
-		Name:    tempModels.Name,
-		Phone:   tempModels.Phone,
-		Email:   tempModels.Email,
-		Message: tempModels.Message,
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"User":    response,
-		"code":    200,
-		"message": "Success recorder added successfully.",
-	})
-}
-
-func UpdateUser(c *gin.Context) {
-	userIDstr := c.Param("id")
-	userID, err := strconv.Atoi(userIDstr)
-	if err != nil {
-		log.Println("Email validation error:", err)
-		c.JSON(http.StatusForbidden, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-	}
-
-	var request createUserRequest
-	err = c.ShouldBindJSON(&request)
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
-		return
-	}
-	// Validate email format
-	if err := validationEmail(request.Email); err != nil {
-		log.Println("Email validation error:", err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code":    400,
-			"message": err.Error(),
-		})
-		return
-
-	}
-	query := "UPDATE users SET name=?, phone=?, email=?,message=? WHERE id=?"
-	_, err = db.Exec(query, request.Name, request.Phone, request.Email, request.Message, userID)
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"code":    200,
-		"message": "Success recorder Updated successfully.",
-	})
-}
-
-// func GetUserDetails(c *gin.Context) {
-
-// 	userIDstr := c.Param("id")
-// 	userID, err := strconv.Atoi(userIDstr)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
-// 	}
-
-// 	type userModel struct {
-// 		ID        int          `db:"id"`
-// 		Name      string       `db:"name"`
-// 		Age       int          `db:"age"`
-// 		Email     string       `db:"email"`
-// 		CreatedAt sql.NullTime `db:"created_at"`
-// 	}
-
-// 	tempModel := userModel{}
-
-// 	query := "SELECT * FROM users WHERE id=?"
-// 	err = db.Get(&tempModel, query, userID)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-// 		return
-// 	}
-
-// 	type tempResponse struct {
-// 		Name     string `json:"name"`
-// 		Age      int    `json:"age"`
-// 		Email    string `json:"email"`
-// 		CreateAt string `json:"createdAt"`
-// 	}
-
-// 	response := tempResponse{
-// 		Name:     tempModel.Name,
-// 		Age:      tempModel.Age,
-// 		Email:    tempModel.Email,
-// 		CreateAt: tempModel.CreatedAt.Time.Format("2006-01-02 15:04:05"),
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"SUCCESS": response})
-// }
-
-// func GetUserListing(c *gin.Context) {
-
-// 	type userModel struct {
-// 		ID       int       `db:"id"`
-// 		Name     string    `db:"name"`
-// 		Age      int       `db:"age"`
-// 		Email    string    `db:"email"`
-// 		CreateAt time.Time `db:"created_at"`
-// 	}
-
-// 	tempModels := []userModel{}
-
-// 	query := "SELECT * FROM users"
-// 	err = db.Select(&tempModels, query)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-// 	}
-
-// 	type tempResponse struct {
-// 		Name     string `json:"name"`
-// 		Age      int    `json:"age"`
-// 		Email    string `json:"email"`
-// 		CreateAt string `json:"created_at"`
-// 	}
-
-// 	response := []tempResponse{}
-
-// 	for _, tempModel := range tempModels {
-// 		response = append(response, tempResponse{
-// 			Name:     tempModel.Name,
-// 			Age:      tempModel.Age,
-// 			Email:    tempModel.Email,
-// 			CreateAt: tempModel.CreateAt.Format("2006-01-02 15:04:05"),
-// 		})
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"SUCCESS": response})
-// }
-
-// func UpdateUserDetails(c *gin.Context) {
-// 	userIDstr := c.Param("id")
-// 	userID, err := strconv.Atoi(userIDstr)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
-// 	}
-
-// 	type updateUserRequest struct {
-// 		Name  string `json:"name" binding:"required"`
-// 		Age   int    `json:"age" binding:"required"`
-// 		Email string `json:"email" binding:"required"`
-// 	}
-
-// 	var request updateUserRequest
-// 	err = c.ShouldBindJSON(&request)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
-// 	}
-
-// 	query := "UPDATE users SET name=?, age=?, email=? WHERE id=?"
-// 	_, err = db.Exec(query, request.Name, request.Age, request.Email, userID)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"SUCCESS": "user data update successfully"})
-// }
-
-// func DeleteUser(c *gin.Context) {
-// 	userIDstr := c.Param("id")
-// 	userID, err := strconv.Atoi(userIDstr)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusBadRequest, gin.H{"FAILED": err})
-// 	}
-
-// 	query := "DELETE FROM users WHERE id=?"
-// 	_, err = db.Exec(query, userID)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		c.JSON(http.StatusInternalServerError, gin.H{"Query Exection Error": err})
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"SUCCESS": "user deleted"})
-// }
